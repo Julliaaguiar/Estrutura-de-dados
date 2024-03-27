3566ce48378197a71ec1fe37bfda84c638f705f7
Julliaaguiar <julliaaguiar8@gmail.com>
2024-03-26 21:28:45 -0300

    Create atividade2.c

---
+++ atividade/atividade2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include < stdlib.h>
+
+
+int principal (int argc, char* argv[]){
+
+    int n = argc - 1;
+    int nunemro = 0;
+    int resultado = 0;
+
+     for ( int  i  =  2 ; i <= n / 2 ; i ++ ){
+        int  n  =  atoi ( argv [ i ])
+    if (numero % i == 0 ){
+    resultado++;
+    }
+     }
+        
+    if (resultado == 0 )
+        printf(" verdadeiro %d /n ",numero);
+    else 
+      printf(" false %d /n ",numero)
+
+
+   
+
+    exit(0);
+}
