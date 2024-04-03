2d1f9cfb94d3c59c90bf9aa631749d84925ac98d
Julliaaguiar <julliaaguiar8@gmail.com>
2024-04-03 20:34:04 -0300

    atividade2

--- Estrutura-de-dados/atividade/atividade2.c
+++ Estrutura-de-dados/atividade/atividade2.c
@@ -5,22 +5,20 @@
 int main (int argc, char* argv[]){
 
     int n = argc - 1;
-    int nunemro = 0;
-    int resultado = 0;
 
      for ( int  i  =  2 ; i <= n / 2 ; i ++ ){
-        int  n  =  atoi ( argv [ i ])
-    if (numero % i == 0 ){
+        int  n  =  atoi ( argv [ i ]);
+    if (n % i == 0 ){
     resultado++;
     }
      }
         
     if (resultado == 0 ){
-        printf(" verdadeiro %d /n ",numero);
-    else 
+        printf(" verdadeiro %d /n ",n);
+  
     }
     else 
-      printf(" false %d /n ",numero)
+      printf(" false %d /n ",n);
 
 
     exit(0);
